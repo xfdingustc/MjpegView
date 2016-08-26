3fb7b62838c06daca27b98455f4e56864fd5ff25
xfdingustc <xfding@cam2cloud.com>
2016-08-26 22:55:21 +0800

    remove mina

--- library/src/main/java/com/xfdingustc/mjpegview/library/MjpegView.java
+++ library/src/main/java/com/xfdingustc/mjpegview/library/MjpegView.java
@@ -9,17 +9,6 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import com.orhanobut.logger.Logger;
-import com.xfdingustc.mjpegview.library.mina.MjpegCodecFactory;
-
-import org.apache.mina.core.future.ConnectFuture;
-import org.apache.mina.core.service.IoConnector;
-import org.apache.mina.core.service.IoHandlerAdapter;
-import org.apache.mina.core.service.IoService;
-import org.apache.mina.core.service.IoServiceListener;
-import org.apache.mina.core.session.IdleStatus;
-import org.apache.mina.core.session.IoSession;
-import org.apache.mina.filter.codec.ProtocolCodecFilter;
-import org.apache.mina.transport.socket.nio.NioSocketConnector;
 
 import java.net.InetSocketAddress;
 
@@ -29,8 +18,7 @@ public class MjpegView extends SurfaceView implements SurfaceHolder.Callback {
 
     private MjpegStream mMjpegStream;
 
-    private IoSession mSession = null;
-    private IoConnector mConnector = null;
+
     private SurfaceHolder mSurfaceHolder;
 
     private boolean mUseMina = false;
@@ -78,91 +66,16 @@ public class MjpegView extends SurfaceView implements SurfaceHolder.Callback {
 
 
     public void startStream(final InetSocketAddress serverAddr) {
-        if (mUseMina) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    startPreviewSocketConnection(serverAddr);
-                }
-            }).start();
-        } else {
-            mMjpegStream.start(serverAddr);
-        }
-    }
 
-    private void startPreviewSocketConnection(InetSocketAddress serverAddr) {
-        mConnector = new NioSocketConnector();
-        mConnector.setConnectTimeoutMillis(5000);
+        mMjpegStream.start(serverAddr);
 
-
-        mConnector.getFilterChain().addLast("codec", new ProtocolCodecFilter(new MjpegCodecFactory()));
-        mConnector.addListener(new IoServiceListener() {
-            @Override
-            public void serviceActivated(IoService ioService) throws Exception {
-
-            }
-
-            @Override
-            public void serviceIdle(IoService ioService, IdleStatus idleStatus) throws Exception {
-
-            }
-
-            @Override
-            public void serviceDeactivated(IoService ioService) throws Exception {
-
-            }
-
-            @Override
-            public void sessionCreated(IoSession ioSession) throws Exception {
-
-            }
-
-            @Override
-            public void sessionClosed(IoSession ioSession) throws Exception {
-
-            }
-
-            @Override
-            public void sessionDestroyed(IoSession ioSession) throws Exception {
-
-            }
-        });
-
-        mConnector.setHandler(new IoHandlerAdapter() {
-            @Override
-            public void messageReceived(IoSession session, Object message) throws Exception {
-//                super.messageReceived(session, message);
-                Bitmap bitmap = (Bitmap) message;
-                drawBitmap(bitmap);
-            }
-        });
-
-        try {
-            ConnectFuture future = mConnector.connect(serverAddr);
-            Logger.t(TAG).d("start connection");
-            future.awaitUninterruptibly();
-            mSession = future.getSession();
-            mSession.getConfig().setReadBufferSize(100000);
-
-            Logger.t(TAG).d("connected");
-
-            // write
-            String request = "GET / HTTP/1.1\r\n" + "Host: " + serverAddr + "\r\n" + "Connection: keep-alive\r\n"
-                + "Cache-Control: no-cache\r\n" + "\r\n";
-
-            mSession.write(request);
-        } catch (Exception e) {
-            Logger.t(TAG).d("connection error");
-        }
     }
 
 
     public void stopStream() {
-        if (mUseMina) {
-            mSession.close();
-        } else {
-            mMjpegStream.stop();
-        }
+
+        mMjpegStream.stop();
+
     }
 
 
